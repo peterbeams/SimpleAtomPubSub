1a9496dde41346ff85a67279fbf9faa6812c5abb
Peter Beams <peter.beams@googlemail.com>
2016-01-29 17:03:42 +0000

    #6 adding handler scanning

---
+++ src/SimpleAtomPubSub/TypeScanningExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using SimpleAtomPubSub.Handler;
+
+namespace SimpleAtomPubSub
+{
+    public static class TypeScanningExtensions
+    {
+        public static Type[] ScanForHandlers(this Assembly assembly)
+        {
+            return assembly.GetTypes()
+                    .Where(t => t.GetInterfaces().Any( x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IHandler<>) ))
+                    .ToArray();
+        }
+    }
+}
