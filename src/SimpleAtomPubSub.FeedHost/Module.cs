e41687d0e6427c8a0e77bdf97a8247b3ff47944a
Peter Beams <peter.beams@googlemail.com>
2016-02-03 12:54:57 +0000

    adding host project based on nancy

---
+++ src/SimpleAtomPubSub.FeedHost/Module.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleAtomPubSub.Feed;
+
+namespace SimpleAtomPubSub.FeedHost
+{
+    public class FeedModule : Nancy.NancyModule
+    {
+        internal static IEventFeed feed;
+        internal static Uri feedUrl = new Uri("http://localhost:12345");
+
+        public FeedModule()
+        {
+            feed = SimpleAtomPubSub.Configure.AsAPublisher("EventStore");
+
+            Get["/"] = _ => feed.GetWorkingFeed(feedUrl);
+            Get["/{name}"] = parameters => feed.GetArchiveFeed(string.Concat("/", parameters.name), feedUrl);
+        }
+    }
+}
